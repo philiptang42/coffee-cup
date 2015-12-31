07b644bb5116b130a7130c185a1cb8d10fb09824
philiptang42 <philiptang42@gmail.com>
2015-12-30 19:21:03 -0500

    part 2 sip method

--- coffee_cup.rb
+++ coffee_cup.rb
@@ -4,8 +4,14 @@ class CoffeeCup
   attr_reader :amount, :capacity
 
   def initialize(amount = 8, capacity = 12)
-    @amount = "#{amount} oz"
-    @capacity = "#{capacity} oz"
+    @amount = amount.to_s + " oz"
+    @capacity = capacity.to_s + " oz"
+  end
+
+  def sip!
+    raw_amount = amount.split('')
+    sipped_amount = raw_amount[0].to_i - 1
+    @amount = sipped_amount.to_s + " oz"
   end
 
 end
